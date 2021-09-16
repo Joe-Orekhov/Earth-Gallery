da93432e5bf0123005ed9a9de9bb9707ccd2685a
Natalie Barba <nbarba2010@gmail.com>
2021-09-16 13:20:23 -0700

    Fixed class -> className

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,19 +11,19 @@ import { Link } from "react-router-dom";
               <h1>
                 {`Welcome to ${selectUser.username}'s Art Gallery`}
             </h1>
-            <div class="tab-home"> 
-              <Link to="/sell"><button class="tab">Sell</button></Link>
-              <Link to="/shop"><button class="tab">Shop</button></Link>
-              <Link to="/cart"><button class="tab">Cart</button></Link>
+            <div className="tab-home"> 
+              <Link to="/sell"><button className="tab">Sell</button></Link>
+              <Link to="/shop"><button className="tab">Shop</button></Link>
+              <Link to="/cart"><button className="tab">Cart</button></Link>
             </div>
-            <Link to="/login"><button class="login-tab-else">Logout</button></Link>
+            <Link to="/login"><button className="login-tab-else">Logout</button></Link>
           </div>
         )
       }else if(selectUser.username === undefined){
         return(
           <div id="header">
             <h1>{`Welcome to Earths Gallery`}</h1>
-            <Link to="/login"><button class="login-tab">Login</button></Link>
+            <Link to="/login"><button className="login-tab">Login</button></Link>
           </div>
         )
       }else{
